7cf96bf659f8bf6c4cf10fd0b8cd9c86d7c8df07
Hoo <sherryho148@gmail.com>
2021-11-29 10:26:32 +0800

    add loading effect

---
+++ src/components/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="spinner-border" style="width: 2rem; height: 2rem;" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "@/assets/scss/abstract/_color.scss";
+
+.spinner-border {
+  margin: 0 auto;
+  color: color.$primary-purple;
+}
+</style>
