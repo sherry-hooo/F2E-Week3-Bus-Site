339d30552c3224b00f9d692d710dab2fffe211a1
Hoo <sherryho148@gmail.com>
2021-11-29 10:28:27 +0800

    revise city info

---
+++ src/assets/data/mapboxToken.js
@@ -0,0 +1,7 @@
+// export default {
+//   accessToken:
+//     'pk.eyJ1Ijoic2hlcnJ5aG8iLCJhIjoiY2t3Mzc4djhxZTFyZDJvczdmeXFpdDY1diJ9.wpSDjKWWn5Nm_f5Y1oeGaA',
+// }
+
+export let myMapToken =
+  "pk.eyJ1Ijoic2hlcnJ5aG8iLCJhIjoiY2t3Mzc4djhxZTFyZDJvczdmeXFpdDY1diJ9.wpSDjKWWn5Nm_f5Y1oeGaA";
