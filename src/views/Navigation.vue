fce23508bda9cb0981a5be1a5249b11ae216cb0b
Hoo <sherryho148@gmail.com>
2021-11-29 10:22:25 +0800

    create navigation page

---
+++ src/views/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <h2>轉乘規劃</h2>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/scss/abstract/_color.scss";
+
+main {
+  min-height: calc(100vh - 80px);
+  background-image: url('~@/assets/img/navigation-sm.png');
+  background-position: bottom;
+  background-size: 100% auto;
+  background-repeat: no-repeat;
+
+  @include breakpoint.tablet {
+    background-image: url('~@/assets/img/navigation.png');
+    background-size: contain;
+  }
+  h2 {
+    color: color.$primary-purple;
+    font-weight: 900;
+    font-size: 36px;
+    margin-bottom: 30px;
+  }
+}
+</style>
