77c075fc428bc177bc9db15f35b2a6abe801c7c7
Hoo <sherryho148@gmail.com>
2021-12-01 19:41:08 +0800

    before rebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Nearby from '../views/Nearby.vue'
-import Navigation from '../views/Navigation.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Nearby from "../views/Nearby.vue";
+import Navigation from "../views/Navigation.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/nearby',
-    name: 'Nearby',
+    path: "/nearby",
+    name: "Nearby",
     props: true,
     component: Nearby,
   },
   {
-    path: '/:mode/:city/:displayName',
-    name: 'Detail',
+    path: "/:mode/:city/:displayName",
+    name: "Detail",
     props: true,
     beforeEnter: (to, from) => {
-      console.log(to, from)
-      if (from.name === 'Nearby') {
-        console.log(this.params.city)
-        return true
+      console.log(to, from);
+      if (from.name === "Nearby") {
+        console.log(this.params.city);
+        return true;
       }
     },
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/Detail.vue'),
+      import(/* webpackChunkName: "about" */ "../views/Detail.vue"),
   },
   {
-    path: '/navigation',
-    name: 'Navigation',
+    path: "/navigation",
+    name: "Navigation",
     component: Navigation,
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
